a100284503027f8ff958e398623d8fdeef1a02de
HanhowChan <45991770+HanhowChan@users.noreply.github.com>
2022-01-07 01:21:58 +0800

    add shader

---
+++ Assets/Chh/Scripts/Chapter12/MGaussianBlur.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MGaussianBlur : MPostEffectsBase
+{
+    public Shader gaussianBlurShader;
+    private Material gaussianBlurMaterial = null;
+    public Material Pmaterial
+    {
+        get
+        {
+            gaussianBlurMaterial = CheckShaderAndCreateMaterial(gaussianBlurShader, gaussianBlurMaterial);
+            return gaussianBlurMaterial;
+        }
+    }
+    // Blur iterations - larger number means more blur.
+    [Range(0, 4)]
+    public int iterations = 3;
+    // Blur spread for each iteration - larger value means more blur
+    [Range(0.2f, 3.0f)]
+    public float blurSpread = 0.6f;
+    [Range(1, 8)]
+    public int downSample = 2;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if(Pmaterial != null)
+        {
+            int rtW = source.width / downSample;
+            int rtH = source.height / downSample;
+            RenderTexture buffer0 = RenderTexture.GetTemporary(rtW, rtH, 0);
+            buffer0.filterMode = FilterMode.Bilinear;
+            Graphics.Blit(source, buffer0);
+            for(int i = 0; i < iterations; i++)
+            {
+                Pmaterial.SetFloat("_BlurSize", 1.0f + i * blurSpread);
+                RenderTexture buffer1 = RenderTexture.GetTemporary(rtW, rtH, 0);
+                //Render the vertical pass
+                Graphics.Blit(buffer0, buffer1, Pmaterial, 0);
+                RenderTexture.ReleaseTemporary(buffer0);
+                buffer0 = buffer1;
+                buffer1 = RenderTexture.GetTemporary(rtW, rtH, 0);
+                //Render the horizontal pass
+                Graphics.Blit(buffer0, buffer1, Pmaterial, 1);
+                RenderTexture.ReleaseTemporary(buffer0);
+                buffer0 = buffer1;
+            }
+            Graphics.Blit(buffer0, destination);
+            RenderTexture.ReleaseTemporary(buffer0); //释放之前分配的缓存
+        }
+        else
+        {
+            Graphics.Blit(source, destination);
+        }
+    }
+    
+}
